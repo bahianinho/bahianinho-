115542ddc0a65719a874e09128de3b9bdc702a6c
bahianinho <77082020+bahianinho@users.noreply.github.com>
2021-01-06 22:36:09 -0300

    Add files via upload

---
+++ scr/help.js
@@ -0,0 +1,62 @@
+const help = (prefix, pushname) => {
+	return `✪═⟪ BOT BAHIANINHO MENU ⟫═✪
+
+*AUTOR : KAKAJOKER*
+*BOT TYPE : GIT CMD*
+
+╔════════════════════
+║ SALVE👋 *${pushname}*
+╠════════════════════
+║╭──❉ *INFO* ❉──
+║│1. ${prefix}info
+║│2. ${prefix}bugreport 
+║│3. ${prefix}runtime
+║│4. ${prefix}join <linkDoGrupo>
+║╰───────────
+║╭──❉ *GROUP MENU* ❉──
+║│1. ${prefix}clone
+║│2. ${prefix}promote <@MarcarMembro>
+║│3. ${prefix}demote <@MarcarAdm>
+║│4. ${prefix}tagall
+║│5. ${prefix}simih <0 ou 1>
+║│6. ${prefix}group <open OU close>
+║│8. ${prefix}setdesc <Sua Descrição>
+║│9. ${prefix}setpp 
+║│10. ${prefix}setname <novoNome>
+║│11. ${prefix}kick <@MarcarMembro>
+║│12. ${prefix}linkgroup
+║╰───────────
+║╭──❉ *MEDIA* ❉──
+║│1. ${prefix}toimg <Converter sticker>
+║│2. ${prefix}sticker
+║│3. ${prefix}ttp <teks>
+║│4. ${prefix}sticker nobg <OFF>
+║│5. ${prefix}tts <CodigiDoIdioma> <frase>
+║│6. ${prefix}url2img <url>
+║│7. ${prefix}wait <Coloque a foto>
+║│8. ${prefix}ocr
+║│9. ${prefix}nulis
+║╰───────────
+║╭──❉ *OWNER MENU* ❉──
+║│1. ${prefix}setprefix <prefix>
+║│2. ${prefix}bc <promoção>
+║│3. ${prefix}setppbot 
+║│4. ${prefix}clone @MarcarMembro
+║╰───────────
+║╭──❉ *IKLAN* ❉──
+║│1. Instagram
+║│ @off_
+║│2. Criador do bot
+║│ https://wa.me/5521997865309
+║╰───────────
+║╭──❉ *PERATURAN* ❉──
+║  1. /appspremium
+║  2. /punheta
+║  3. /botst (fazerbot)
+╠════════════════════
+ KAKAJOKER
+  _*Bahianinho botᵏ፝⃟ ⚡*_
+╚════════════════════`
+}
+
+exports.help = help
